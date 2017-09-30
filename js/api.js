55125d00a62c9ca876f87b29d2bd6519529957f2
EliteToken <tokenelite@gmail.com>
2017-09-30 15:58:20 +0300

    Site

---
+++ js/api.js
@@ -0,0 +1,24 @@
+$(function() {
+        try {
+            var Web3 = require('web3');
+            var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/"));
+            var version = web3.version.api;
+            var count;
+
+            $.getJSON('http://api.etherscan.io/api?module=contract&action=getabi&address=0xF6d300C651E892E48718689b426757fbF01FAb2b', function (data) {
+                var contractABI = "";
+                contractABI = JSON.parse(data.result);
+                if (contractABI != '') {
+                    var MyContract = web3.eth.contract(contractABI);
+                    var myContractInstance = MyContract.at('0xF6d300C651E892E48718689b426757fbF01FAb2b');
+                    $('#sold').html(myContractInstance.supply().toFixed(8));
+                    $('#used').html(myContractInstance.wishes().toFixed(8));
+                    $('#exist').html((myContractInstance.supply() - (myContractInstance.wishes() * 100000000)).toFixed(8));
+                } 
+                else {
+                    console.log("Error" );
+                }            
+            }.bind(this));
+
+        } catch (err) { }
+});
