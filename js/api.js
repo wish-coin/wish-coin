bf0f45ac1157cc33587cb704cf385d688f5a4af0
wish-coin <32414522+wish-coin@users.noreply.github.com>
2017-09-30 16:01:27 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -5,7 +5,7 @@ $(function() {
             var version = web3.version.api;
             var count;
 
-            $.getJSON('http://api.etherscan.io/api?module=contract&action=getabi&address=0xF6d300C651E892E48718689b426757fbF01FAb2b', function (data) {
+            $.getJSON('https://api.etherscan.io/api?module=contract&action=getabi&address=0xF6d300C651E892E48718689b426757fbF01FAb2b', function (data) {
                 var contractABI = "";
                 contractABI = JSON.parse(data.result);
                 if (contractABI != '') {
